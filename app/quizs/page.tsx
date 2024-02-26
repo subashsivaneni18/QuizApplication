eba0c5a64a628f17ae0c335690251a0bbfc0813e
subashsivaneni18 <satyasaisubash8@gmail.com>
2024-02-26 16:49:20 +0530

    seventh

--- app/quizs/page.tsx
+++ app/quizs/page.tsx
@@ -1,18 +1,36 @@
 "use client";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Navbar from "../components/Navbar";
 import QuizCard from "../components/QuizCard";
-import useSWR from "swr";
-import fetcher from "@/libs/fetcher";
+
 import { Quiz } from "@prisma/client";
 import { redirect, useRouter } from "next/navigation";
 import { useSession } from "next-auth/react";
+import axios from "axios";
 
 export const dynamic = "force-dynamic";
 const QuizsPage = () => {
-  const { data } = useSWR<Quiz[]>(`/api/Quizs`, fetcher);
+
+  const [ quizs,setQuizs] =useState<Quiz[]>([])
   const router = useRouter()
   const {status} = useSession()
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get("/api/Quizs");
+        setQuizs(response.data);
+      } catch (error) {
+        // Handle error if necessary
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+
+  console.log(quizs)
 
   if(status==='loading')
   {
@@ -34,7 +52,7 @@ const QuizsPage = () => {
     <div className="relative">
       <Navbar />
       <div className="absolute top-[80px] z-10 p-5 grid grid-cols-1 md:grid-cols-3 gap-4">
-        {data?.map((quiz) => (
+        {quizs?.map((quiz) => (
           <div
             key={quiz.id}
             onClick={() => router.push(`/quiz/${quiz.id}/rules`)}
